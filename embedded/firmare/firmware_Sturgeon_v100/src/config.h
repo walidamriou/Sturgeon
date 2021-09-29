766f174ad1a3cceff4cbc8ed261921a7bea43e0f
Walid Amriou <walidamriou@gmail.com>
2021-09-29 14:50:16 +0100

    Create config.h

---
+++ embedded/firmare/firmware_Sturgeon_v100/src/config.h
@@ -0,0 +1,19 @@
+/**
+ * @file config.h
+ * @brief header file of config
+ *
+ * @version 1.0.0
+ * 
+ * @author Walid Amriou
+ * @date 09/29/2021
+ * 
+ * 
+ */
+
+#ifndef CONFIG_H_INCLUDED
+#define CONFIG_H_INCLUDED
+
+//#define arduino_uno
+#define arduino_mega
+
+#endif
