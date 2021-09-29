13bcc80b1c373aaaf69f34333f45ea665c753cfc
Walid Amriou <walidamriou@gmail.com>
2021-09-29 15:11:58 +0100

    Update config.h

--- embedded/firmare/firmware_Sturgeon_v100/src/config.h
+++ embedded/firmare/firmware_Sturgeon_v100/src/config.h
@@ -8,6 +8,14 @@
  * @date 09/29/2021
  * 
  * 
+ * 
+ *  ____________                                 _________________________________                ___________________________________________
+ * |            | <=== motor attack signal ==== |                                 |              |                                           |    
+ * |    FAN     |                               |  Main board                     | <=========== |  Display board                            |         
+ * |____________| ==== motor speed signal ====> | (Buck converter DC/DC with MCU) |              |  (MCU, 1x Button, 4x LEDs, and !x buzzer) |
+ *                                              |_________________________________|              |___________________________________________|
+ *
+ *
  */
 
 #ifndef CONFIG_H_INCLUDED
